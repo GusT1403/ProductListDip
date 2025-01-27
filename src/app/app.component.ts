de48925ab4148bdf9124496c59e606e166360415
GusT <gustarmi@outlook.com>
2025-01-26 22:39:05 -0400

    Homework 2 diplomado: decorators Input Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,14 @@ import { ProductListComponentComponent } from './product-list-component/product-
 })
 export class AppComponent {
   title = 'projectDip2';
+  entryMessage = 'Product 1';
+  entryMessage2 = 'Product 2';
+  entryMessage3 = 'Product 3';
+  entryImg = './public/img11.jpg';
+
+  counter = 0;
+  
+  add(value: number) {
+    this.counter += value;
+  }
 }
--- src/app/product-list-component/product-list-component.component.ts
+++ src/app/product-list-component/product-list-component.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-product-list-component',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './product-list-component.component.css'
 })
 export class ProductListComponentComponent {
+  @Input () message : string = '';
+  @Input () img : string = '';
+
+
+  @Output() purchase = new EventEmitter<number>();
+
+  addToCart() {
+    this.purchase.emit(1);
+  }
 
 }
